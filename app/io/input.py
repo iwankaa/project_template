a9e2e859eb437d805d89d69337bdbde1c61504d4
Ivanna Lutsiuk <lutsiukivanna268@gmail.com>
2024-03-16 19:08:44 +0200

    Added docstring for functions

--- app/io/input.py
+++ app/io/input.py
@@ -2,15 +2,39 @@ import pandas
 
 
 def get_console_input():
+    """
+    Function which gets data from user
+
+    Returns:
+        string: the data from user
+    """
     return input("Enter your data :")
 
 
 def get_data_from_file(filename):
+    """
+    Function which gets data from file
+
+    Args:
+        filename (str): The name of the file to  read.
+
+    Returns:
+        string: the data from file
+    """
     file = open(filename, "r")
     read_content = file.read()
     return read_content
 
 
 def get_data_pandas(filename):
+    """
+    Function that uses pandas to get data from file as a dataframe
+
+    Args:
+        filename (str): The name of the file to  read.
+
+    Returns:
+        string: the data from file as a dataframe
+    """
     dataframe = pandas.read_csv(filename)
     return dataframe
--- app/io/output.py
+++ app/io/output.py
@@ -1,6 +1,24 @@
 def output_to_console(text_to_out):
+    """
+    Function which makes output to console
+
+    Args:
+        text_to_out (str): Text to be displayed in console
+
+    Returns:
+        None. This function doesn't return any value.
+    """
     print(text_to_out)
 
 def output_to_file(filename):
+    """
+    Function which writes text to a file.
+
+    Args:
+        filename (str): The name of the file to write to.
+
+    Returns:
+        None. This function doesn't return any value.
+    """
     with open(filename, 'w') as file2:
         file2.write('Programming is Fun.')
